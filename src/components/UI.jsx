ac3c3d017ca1205943db4e1c53d90e906616009e
Adrish Basak <adrishbasak003@gmail.com>
2024-09-22 19:56:25 +0530

    change 2

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -89,54 +89,54 @@ export const UI = () => {
         <div className="relative">
           <div className="bg-white/0  animate-horizontal-scroll flex items-center gap-8 w-max px-8">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
+              Bepo's
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
+              animated
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
-              Three.js
+              book
             </h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
+              Slider
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
+              Bepo's
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
+              animated
             </h2>
             <h2 className="shrink-0 text-white text-13xl font-bold">
-              Practice
+              book
             </h2>
             <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Creative
+              Slider
             </h2>
           </div>
           <div className="absolute top-0 left-0 bg-white/0 animate-horizontal-scroll-2 flex items-center gap-8 px-8 w-max">
             <h1 className="shrink-0 text-white text-10xl font-black ">
-              Wawa Sensei
+              Bepo's
             </h1>
             <h2 className="shrink-0 text-white text-8xl italic font-light">
-              React Three Fiber
+              animated
             </h2>
             <h2 className="shrink-0 text-white text-12xl font-bold">
-              Three.js
+              book
             </h2>
             <h2 className="shrink-0 text-transparent text-12xl font-bold italic outline-text">
-              Ultimate Guide
+              Slider
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-medium">
-              Tutorials
+              Bepo's
             </h2>
             <h2 className="shrink-0 text-white text-9xl font-extralight italic">
-              Learn
+              animated
             </h2>
             <h2 className="shrink-0 text-white text-13xl font-bold">
-              Practice
+              book
             </h2>
             <h2 className="shrink-0 text-transparent text-13xl font-bold outline-text italic">
-              Creative
+              Slider
             </h2>
           </div>
         </div>
