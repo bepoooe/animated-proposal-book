13c41fcdbf666c525b57cbf16e8adb136c12698c
Adrish Basak <adrishbasak003@gmail.com>
2024-09-22 20:45:45 +0530

    change 3

--- src/components/UI.jsx
+++ src/components/UI.jsx
@@ -52,7 +52,7 @@ export const UI = () => {
       <main className=" pointer-events-none select-none z-10 fixed  inset-0  flex justify-between flex-col">
         <a
           className="pointer-events-auto mt-10 ml-10"
-          href="https://lessons.wawasensei.dev/courses/react-three-fiber"
+          href="https://lessons.wawasensei.dev/courses/react-three-fiber" //remove this later or link it later
         >
           <img className="w-20" src="/images/wawasensei-white.png" />
         </a>
